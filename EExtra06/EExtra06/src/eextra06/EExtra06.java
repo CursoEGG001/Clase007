f080dad3ad920de02b3e1e3f18aa8c63810e2582
CursoEGG001 <soygecko@gmail.com>
2023-04-06 12:33:31 -0300

    Ejercicio 6 modificado y ampliado

--- EExtra06/EExtra06/src/eextra06/EExtra06.java
+++ EExtra06/EExtra06/src/eextra06/EExtra06.java
@@ -18,5 +18,12 @@ public class EExtra06 {
     public static void main(String[] args) {
          Rectangulo rectangulo1 = new Rectangulo(11, 687);
         System.out.println("El área del rectángulo es: " + rectangulo1.calcular_area());
+        
+        Rectangulo unrectangulito01 = new Rectangulo(1,1);
+        
+        unrectangulito01.setLado1(4);
+        
+        System.out.println("El rectangulo "+ unrectangulito01.toString() + " es ahora de " + unrectangulito01.getLado1() + " por " + unrectangulito01.getLado2());
+
     }
 }
