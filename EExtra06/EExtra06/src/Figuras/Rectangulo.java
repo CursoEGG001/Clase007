2d1eac57519dacbf798d4b64412f7d975b318cad
CursoEGG001 <soygecko@gmail.com>
2023-04-06 12:44:45 -0300

    Cambios en el constructor y ampliación de toString()

--- EExtra06/EExtra06/src/Figuras/Rectangulo.java
+++ EExtra06/EExtra06/src/Figuras/Rectangulo.java
@@ -20,4 +20,31 @@ public class Rectangulo {
     public double calcular_area() {
         return lado1 * lado2;
     }
+
+    public double getLado1() {
+        return lado1;
+    }
+
+    public void setLado1(double lado1) {
+        this.lado1 = lado1;
+    }
+
+    public double getLado2() {
+        return lado2;
+    }
+
+    public void setLado2(double lado2) {
+        this.lado2 = lado2;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Rectangulo{");
+        sb.append("lado1=").append(lado1);
+        sb.append(", lado2=").append(lado2);
+        sb.append('}');
+        return sb.toString();
+    }
+    
 }
